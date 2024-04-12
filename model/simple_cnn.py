3f3cfe3299db9bef7b875ab4f0da9b576aeb7aab
anonymous <anonymisedreview@gmail.com>
2024-04-12 15:54:49 +0200

    correction on model file

--- model/simple_cnn.py
+++ model/simple_cnn.py
@@ -1,5 +1,5 @@
 """
-Just a simple CNN that directly reads on the audio file.
+Just a simple CNN that directly reads on the audio file / spectrogram.
 """
 
 import tensorflow as tf
@@ -22,42 +22,61 @@ cnn_default_params = {
     "use_batch_norm": True,
     "head_dense": 64,
     "dense_activation": "relu",
+    "global_pooling": "max",
 
     "sr": 44100,
     "audio_slice": 0.5,
 }
 
 class SimpleCNN():
-    """ raw audio """
-    def __init__(self, params = {}):
+    def __init__(self, input_shape, params = {}, detect_encoder = False):
         self.name = "simple"
 
         self.params = cnn_default_params
         self.params.update(params)
 
-        X = Input((int(self.params["sr"] * self.params["audio_slice"]), 2))
+        X = Input((input_shape))
         Y = X
 
         for h, s, k in zip(self.params["hidden"], self.params["strides"], self.params["kernel"]):
-            Y = Conv1D(h, kernel_size=k, strides=s, padding='same', activation=self.params["conv_activation"])(Y)
+            if self.params['use_maxpool']:
+                Y = Conv1D(h, kernel_size=k,
+                        padding='same', activation=self.params["conv_activation"])(Y)
+                Y = MaxPooling1D(pool_size=s)(Y)
+            else:
+                Y = Conv1D(h, kernel_size=k, strides=s,
+                        padding='same', activation=self.params["conv_activation"])(Y)
+
             if self.params["use_batch_norm"]:
                 Y = BatchNormalization()(Y)
 
-        Y = GlobalMaxPooling1D()(Y)
+        if self.params["global_pooling"] == "average":
+            Y = GlobalAveragePooling1D()(Y)
+        else:
+            Y = GlobalMaxPooling1D()(Y)
+
         Y = Dropout(0.2)(Y)
 
-        self.fc = Dense(self.params["head_dense"], activation = self.params["dense_activation"])
-        self.fc_out = Dense(1, activation = 'sigmoid')
+        if self.params["head_dense"] > 0:
+            self.fc = Dense(self.params["head_dense"], activation = self.params["dense_activation"])
+            Y = self.fc(Y)
+            Y = Dropout(0.2)(Y)
 
-        Y = self.fc(Y)
-        Y = Dropout(0.2)(Y)
-        Y = self.fc_out(Y)
+        self.fc_out = Dense(1, activation = 'sigmoid', name="deepfake")
+
+        if not detect_encoder:
+            Y = self.fc_out(Y)
+        else:
+            self.fc_encoder = Dense(detect_encoder, activation = 'softmax', name="encoder")
+            Y1 = self.fc_out(Y, )
+            Y2 = self.fc_encoder(Y)
+            Y = [Y1, Y2]
 
         self.m = Model(X, Y)
 
 
+
 class SimpleSpectrogramCNN():
-    """ spectrogram based model """
     def __init__(self, input_shape, params = {}, detect_encoder = False, get_embedder = False):
         self.name = "specnn"
 
